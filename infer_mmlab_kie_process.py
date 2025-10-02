7b0b0b57873939ec2414072ffee40ef255a669bc
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2025-10-02 14:46:42 +0200

    fix: missing base class call to init_long_process()

--- infer_mmlab_kie_process.py
+++ infer_mmlab_kie_process.py
@@ -208,6 +208,7 @@ class InferMmlabKie(dataprocess.C2dImageTask):
 
     def init_long_process(self):
         self._load_model()
+        super().init_long_process()
 
     def run(self):
         # Core function of your process
